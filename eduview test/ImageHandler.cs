6928ded87ea462bd45a35713f66d9390c2ed9ea3
edi <edi@DESKTOP-NT3NJR0>
2024-08-04 22:07:16 +0300

    Add project files.

---
+++ eduview test/ImageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace EduViewStudent
+{
+    public class ScreenshotHandler
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+
+        public Bitmap GetScreenshot()
+        {
+            Size size = GetScreenData();
+            Console.WriteLine(size.ToString());
+            Bitmap capture = CaptureScreen(size);
+            return capture;
+        }
+
+        private Size GetScreenData()
+        {
+            Size size = new Size(GetSystemMetrics(0), GetSystemMetrics(1));
+            return size;
+        }
+
+        private Bitmap CaptureScreen(Size screenData)
+        {
+            // Create a bitmap with the specified size
+            Bitmap capture = new Bitmap(screenData.Width, screenData.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics captureGraphics = Graphics.FromImage(capture))
+            {
+                // Capture the screen content
+                captureGraphics.CopyFromScreen(0, 0, 0, 0, screenData);
+            }
+
+            return capture;
+        }
+    }
+}
---
+++ eduview test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace EduViewStudent
+{
+    public partial class Program
+    {
+        
+
+        public static int Main()
+        {
+            ScreenshotHandler sh = new ScreenshotHandler();
+
+            Bitmap capture = sh.GetScreenshot();
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, "screenshot.png");
+            capture.Save(filePath, ImageFormat.Png);
+
+            return 0;
+        }
+    }
+}
